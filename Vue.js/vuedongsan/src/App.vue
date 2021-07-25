5c8464bf6602a32fedb6cf01b69102590b3c8923
chanin <chanin.shim@gmail.com>
2021-07-25 22:51:39 +0900

    21.07.25 vue 연습

---
+++ Vue.js/vuedongsan/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      <h4>xx 원룸</h4>
+      <p>50 만원</p>
+      <h4>xx 원룸</h4>
+      <p>80 만원</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
